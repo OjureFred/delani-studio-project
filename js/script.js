63b0c147879f87c6597f1cb36a7fcb56bf8e19b6
Fred Ojure <fredojure@hotmail.com>
2020-05-17 18:35:59 +0300

    Added overlay for the first image in portfolio

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+//Toggle between image and description for what we do section
+
+
+//Hover effet for portfolio section
+
+
+//Manage user form with mail chimp
+
+
+});
