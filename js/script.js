1f392a7fec54b6b2cabb5f7bbe8a128a5f937a88
Fred Ojure <fredojure@hotmail.com>
2020-05-19 18:54:49 +0300

    Finished off contact form

--- js/script.js
+++ js/script.js
@@ -16,50 +16,12 @@ $(this).find(".overlay-card").hide();
 });
 
 
-//Manage user form with mail chimp
-//Initialize form
-$('#subscribe-form').MailChimpForm({
-    url: '//jqueryscript.us6.list-manage.com/subscribe/post?u=d4d8a0f45ce17e5ef2708771f&amp;id=06a33bc6c9',
-    fields: '0:EMAIL,1:FULLNAME',
-    submitSelector: '#submit-form',
-    customMessages: {
-        E001: 'This field can not be empty',
-        E003: 'Please enter a valid email address',
-      }
-    });
-
-//Callback functions
-$('#subscribe-form').MailChimpForm({
-    onFail: (message) => {
-    console.error(message);
-    },
-    onOk: (message) => {
-       console.log(message);
-     }
-     });
-
-//Event handlers
-const $formInput = $('input');
-/**
-* @param element
-* @param {String} color
-*/
-function addBorder(element, color) {
-element.css({'border': `1px solid ${color}`});
-}
-/**
-* mc:input:error event handler
- */
-$formInput.on('mc:input:error', function () {
- console.log('mc:input:error event fired');
- addBorder($(this), 'red');
-});
-/**
-* mc:input:ok event handler
-*/
-$formInput.on('mc:input:ok', function () {
-console.log('mc:input:ok event fired');
-addBorder($(this), 'green');
-});
+//Processing form data
+ $("#contact").submit(function(e) {
+ e.preventDefault();
+ var fullname = $("#fullname").val();
+ var email =$("#email").val();
+ alert("Thank you " + fullname + " we have recieved your message and will get back to you on " + email);
+ });
 
 });
